d0724988b667c8e2dc788c4a061615fc220f05cf
samira <s01tani.samirasoltani@gmail.com>
2021-03-01 17:57:48 +0330

    add new address

--- src/app/addresses/addresses.module.ts
+++ src/app/addresses/addresses.module.ts
@@ -16,12 +16,13 @@ import {GridModule,
 	ModalService,
 	PlaceholderModule,
 	CheckboxModule,
-	InlineLoadingModule
+	InlineLoadingModule,
+	LoadingModule
 	} from 'carbon-components-angular';
 import { AddressListItemComponent } from './address-list/address-list-item/address-list-item.component';
 import { AddComponent } from './address-list/add/add.component';
 import { ReactiveFormsModule} from '@angular/forms';
-import { DeleteModule,AddModule, EditModule , ArrowLeftModule , CloseModule , CheckmarkModule ,ChevronRightModule, PhoneModule} from "@carbon/icons-angular";
+import { DeleteModule,AddModule, EditModule , ArrowLeftModule , CloseModule , CheckmarkModule ,ChevronRightModule, PhoneModule, WarningModule, CheckmarkOutlineModule} from "@carbon/icons-angular";
 import { NeighborMailboxComponent } from './neighbor-mailbox/neighbor-mailbox.component';
 import { ConfirmModalComponent } from './address-list/confirm-modal/confirm-modal.component';
 @NgModule({
@@ -50,7 +51,10 @@ import { ConfirmModalComponent } from './address-list/confirm-modal/confirm-moda
 		PlaceholderModule,
 		CheckboxModule,
 		PhoneModule,
-		InlineLoadingModule
+		InlineLoadingModule,
+		WarningModule,
+		CheckmarkOutlineModule,
+		LoadingModule
 	
 	]
 })
